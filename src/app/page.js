49a03966f9400506965c2941b14df30f74d50795
Muzammil-Mazhar <140798237+Muzammil-Mazhar@users.noreply.github.com>
2024-05-25 20:23:56 +0100

    new update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import ServiceSection from "@/components/ServiceSection";
+
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>DigiMarkistan 1122</h1>
      <h1>DigiMarkistan 11222</h1>
+  <ServiceSection/>
     </main>
   );
 }
---
+++ src/components/ServiceSection.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+
+function ServiceSection(){
+    return(
+        <div>
+         <Link href="./Services">Services</Link>
+        </div>
+    )
+}
+
+
+export default ServiceSection;
